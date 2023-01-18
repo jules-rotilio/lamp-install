1d05ea6290c3b03d112f998298c86b73e0c0d229
jules-rotilio <79381489+jules-rotilio@users.noreply.github.com>
2023-01-18 12:39:07 +0100

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install apache2 mariadb-server mariadb-client \
+	php libapache2-mod-php php-cli php-mysql php-zip php-curl php-xml phpmyadmin wget -y
+/
+
+sudo systemctl enable apache2
+sudo systemctl enable mariadb
+
+mysql_secure_installation
